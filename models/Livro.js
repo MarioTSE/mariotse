8db9b0937fc429932446cc3649289efad260d993
MarioTSE <treina.coede@tsecurso1-14.tse.gov.br>
2016-12-12 11:20:50 -0200

    Inclusão da classe livro.

---
+++ models/Livro.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+
+var LivroSchema = new mongoose.Schema({
+  titulo: String,
+  autor : String,
+  ano : Number,
+  editora : String
+});
+
+module.exports = mongoose.model('Aluno', AlunoSchema);
