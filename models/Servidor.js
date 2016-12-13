4f226575a0546c630fc6bc145289d06d2fee0d65
MarioTSE <treina.coede@tsecurso1-14.tse.gov.br>
2016-12-13 10:09:08 -0200

    Criação de CRUDs para as classes Servidor e EstadoCivil.

--- models/Servidor.js
+++ models/Servidor.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-var EstadoCivil = require('EstadoCivil');
+//var EstadoCivil = require('EstadoCivil');
 
 var ServidorSchema = new mongoose.Schema({
     matricula: Number,
@@ -7,11 +7,11 @@ var ServidorSchema = new mongoose.Schema({
     dataNascimento: Date,
     sexo: String,
     //estadoCivil    : {s:"solteiro", c : "casado"},
-//    estadoCivil: {
-//        codigo: String,
-//        descricao: String
-//    },
-    estadoCivil:EstadoCivil,
+    estadoCivil: {
+        codigo: String,
+        descricao: String
+    },
+//    estadoCivil:EstadoCivil,
     cepResidencia: String,
     logradouroResidencia: String,
     numeroLogradouroResidencia: String,
